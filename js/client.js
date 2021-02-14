d77cf67586c47bd19b7c6c6122eb457fddb65ec3
Ashish Sharma <sharmaashishsde@gmail.com>
2021-02-14 19:54:56 +0530

    feat:backend developed
    backend of the application is developed

---
+++ js/client.js
@@ -0,0 +1,35 @@
+const socket=io('http://localhost:8000');
+
+const form=document.getElementById('send-container')
+const MessageInput=document.getElementById('messageInput')
+var messageContainer=document.querySelector('.container')
+
+const append=(message,position)=>{
+    const messageElement=document.createElement('div');
+    messageElement.innerText=message;
+    messageElement.classList.add('message')
+    messageElement.classList.add(position)
+    messageContainer.append(messageElement);
+}
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const message=MessageInput.value;
+    append(`You: ${message}`,"right");
+    socket.emit('send',message);
+    MessageInput.value='';
+})
+
+const name=prompt("Enter your name");
+socket.emit('new-user-joined',name);
+
+socket.on('user-joined',name=>{
+    append(`${name} joined the chat`,"right");
+})
+
+socket.on('recieve',data=>{
+    append(`${data.name}:${data.message}`,"left");
+})
+
+socket.on('left',name=>{
+    append(`${data.name} left the chat` ,"left")
+})
